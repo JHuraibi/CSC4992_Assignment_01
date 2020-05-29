502ec894760483d5ce628fb37d94a5404bc21a21
JHuraibi <fh1328@wayne.edu>
2020-05-28 23:01:12 -0400

    Renaming and LF
    TryCatch -> TryExcept
    Added proper line ending to .py files

--- Question_03_TryExcept.py
+++ Question_03_TryExcept.py
@@ -1,6 +1,6 @@
 # Author: Jamal Huraibi, fh1328
 # Assignment 1
-# Question 3
+# Question 3 (Alternate Solution for Validation)
 
 
 class SaleHelper:
@@ -26,33 +26,33 @@ class SaleHelper:
         self.__get_input_tip_rate()  # Get tip rate (as %)
     
     def __get_input_bill(self):
-        print("Please Enter the Bill Amount: $", end="")  # Prompt user for bill subtotal
+        print("Please Enter the Bill Amount: $", end="")                        # Prompt user for bill subtotal
         value = input()
         
         while not self.check_valid(value):  # Ensure bill subtotal valid
-            print("\nThe value entered was invalid.", end='\n')  # Reattempt if provided invalid input
+            print("\nThe value entered was invalid.", end='\n')                 # Reattempt if provided invalid input
             print("Please Enter the Bill Amount: $", end="")
             value = input()
         
         self.bill_subtotal = float(value)
     
     def __get_input_sales_tax_rate(self):
-        print("Please Enter the Sales Tax Rate: ", end="")  # Prompt user for sales tax rate (%)
+        print("Please Enter the Sales Tax Rate: ", end="")                      # Prompt user for sales tax rate (%)
         value = input()
         
-        while not self.check_valid(value):  # Ensure tax rate valid
-            print("\nThe value entered was invalid.", end='\n')  # Reattempt if provided invalid input
+        while not self.check_valid(value):                                      # Ensure tax rate valid
+            print("\nThe value entered was invalid.", end='\n')                 # Reattempt if provided invalid input
             print("Please Enter the Sales Tax: ", end="")
             value = input()
         
         self.sales_tax_rate = float(value)
     
     def __get_input_tip_rate(self):
-        print("Please Enter the Tip Rate: ", end="")  # Prompt user for tip rate (%)
+        print("Please Enter the Tip Rate: ", end="")                            # Prompt user for tip rate (%)
         value = input()
         
-        while not self.check_valid(value):  # Ensure tip rate valid
-            print("\nThe value entered was invalid.", end='\n')  # Reattempt if provided invalid input
+        while not self.check_valid(value):                                      # Ensure tip rate valid
+            print("\nThe value entered was invalid.", end='\n')                 # Reattempt if provided invalid input
             print("Please Enter the Tip Rate: ", end="")
             value = input()
         
@@ -63,16 +63,16 @@ class SaleHelper:
         sales_tax_rate = self.sales_tax_rate
         tip_rate = self.tip_rate
         
-        self.tax_amount = bill_subtotal * (sales_tax_rate / 100)  # Subtotal times tax rate in %
-        self.tip_amount = bill_subtotal * (tip_rate / 100)  # Subtotal times tip rate in %
+        self.tax_amount = bill_subtotal * (sales_tax_rate / 100)                # Subtotal times tax rate in %
+        self.tip_amount = bill_subtotal * (tip_rate / 100)                      # Subtotal times tip rate in %
         
-        self.bill_total = bill_subtotal + self.tax_amount + self.tip_amount  # Add bill subtotal, tax, and tip
+        self.bill_total = bill_subtotal + self.tax_amount + self.tip_amount     # Add bill subtotal, tax, and tip
     
     def print_bill(self):
         print("Dear User,")
-        print("Your bill amount is: $", self.bill_subtotal)  # Print bill subtotal
-        print("Your tax amount is: $", self.tax_amount)  # Print tax amount
-        print("Your tip amount is: $", self.tip_amount)  # Print tip amount
+        print("Your bill amount is: $", self.bill_subtotal)                     # Print bill subtotal
+        print("Your tax amount is: $", self.tax_amount)                         # Print tax amount
+        print("Your tip amount is: $", self.tip_amount)                         # Print tip amount
         print("****************************************")
         # Total bill amount, to 2 decimal places
         print("Your Total Bill, including sales tax and tip is ${:.2f}".format(self.bill_total))
